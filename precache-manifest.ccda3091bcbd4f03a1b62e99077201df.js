fa81d46493fd6dd7edfdfc8ea2686743cf2dbf07
Norayr <hovakimyan.norayr1@gmail.com>
2019-06-09 22:38:42 +0400

    Updates

---
+++ precache-manifest.ccda3091bcbd4f03a1b62e99077201df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a8f97465fa79bdff8fb855c875acb4",
+    "url": "/quizz_app/index.html"
+  },
+  {
+    "revision": "566cff85590adc6dabb6",
+    "url": "/quizz_app/static/css/main.5ad612d8.chunk.css"
+  },
+  {
+    "revision": "f96326d9d8732a435b77",
+    "url": "/quizz_app/static/js/2.d72b7372.chunk.js"
+  },
+  {
+    "revision": "566cff85590adc6dabb6",
+    "url": "/quizz_app/static/js/main.f884e1a6.chunk.js"
+  },
+  {
+    "revision": "fc7cd6aeb79c925ecb59",
+    "url": "/quizz_app/static/js/runtime~main.20e1070d.js"
+  }
+]);
